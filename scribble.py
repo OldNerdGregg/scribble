cd223ee49135bef3007edf01abf3d8557ad52fc0
Gregg's Pixel3 <g.russ77@gmail.com>
2019-10-19 14:12:41 -0400

    quitting implemented

--- scribble.py
+++ scribble.py
@@ -11,7 +11,9 @@ def menu():
     choice = str(input('Command: ')).capitalize() # Capitalize to make input
         # choices simpler.
     print("You typed: " + choice)
-
+    if choice == "Q":
+        global keep_running 
+        keep_running = False
 
 # Start the program
 main()
