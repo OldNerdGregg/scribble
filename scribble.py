07c68af2d5859213d79977a2ec74d2552495141b
Gregg's Pixel3 <g.russ77@gmail.com>
2019-10-19 15:07:04 -0400

    added version printing command

--- scribble.py
+++ scribble.py
@@ -1,5 +1,6 @@
 script_version = '0.0.1a'
 keep_running = True
+current_scroll = "PAPYRUS.txt"
 
 def main():
     while keep_running == True:
@@ -8,12 +9,21 @@ def main():
         print("Program exiting!")
 
 def menu():
+    print("[Insribing To: " + current_scroll + " ]")
     choice = str(input('Command: ')).capitalize() # Capitalize to make input
         # choices simpler.
     print("You typed: " + choice)
     if choice == "Q":
         global keep_running 
-        keep_running = False
+        keep_running = False # Quits program
+    elif choice == "V":
+        get_version()
+
+def get_version():
+    print(15 * '#')
+    print("Script Version: " + script_version) 
+    print(15 * '#')
+    print("")
 
 # Start the program
 main()
